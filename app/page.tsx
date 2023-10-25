be2e80b2b6ee6f842e470d9d5fc2cdc57a1d01d5
Brownson <esitibrownson@gmail.com>
2023-10-25 09:12:12 +0100

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import GotoBanner from "@/components/GotoBanner";
 import Hero from "@/components/Hero";
+import Reasons from "@/components/Reasons";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
 
@@ -8,8 +9,9 @@ export default async function Home() {
     <main className="h-[1000dvh]">
       <Hero mainTitle="All in one" supportTitle="Solution Hub" description="A specialist who can grow your business, companies and enterprises" CAC={true}/>
       <Services />
-      <Testimonials />
+      <Reasons/>
       <GotoBanner />
+      <Testimonials />
     </main>
   )
 }
